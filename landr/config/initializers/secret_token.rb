60ba0cdc146e6a52c92f6eec6f441695af3aed86
nixonkevin <nixonkevin@hotmail.com>
2014-02-26 18:37:05 -0500

    added devise

---
+++ landr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Landr::Application.config.secret_key_base = '20dc6cc38c4c382a7e645273acef281f24e2c9a7e797ab96910edd072efbde25e57d3b30f0dfbe67c3da1b271f95bc0f924e16a259492c326278383c62089dd9'
